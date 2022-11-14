7eefd5d2d32fca82290c98270d7531d66ea26f76
Ben Bushell <ben.bushell87@icloud.com>
2022-11-14 15:56:53 +0700

    update connection

--- db/connection.js
+++ db/connection.js
@@ -8,9 +8,9 @@ const config =
       }
     : {};
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
+// require('dotenv').config({
+//   path: `${__dirname}/../.env.${ENV}`,
+// });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
