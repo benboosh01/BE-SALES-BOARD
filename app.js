8ed36fecf40e5b27e0726af33bf7d3e560059b1f
Ben Bushell <ben.bushell87@icloud.com>
2022-11-15 14:19:43 +0700

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const controllers = require('./controllers/score-board.controllers');
 const errorHandlers = require('./controllers/error-handling.controllers');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', controllers.getApi);
