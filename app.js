efae1e522a4f5a6e10a22fb1d5fa87c6fe0beeb4
Ben Bushell <ben.bushell87@icloud.com>
2022-11-15 13:25:40 +0700

    add wrong url error handle

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ app.get('/api/sales_types', controllers.getSalesTypes);
 app.get('/api/users', controllers.getUsers);
 app.get('/api/sales', controllers.getSales);
 app.post('/api/users', controllers.postUser);
-app.patch('/api/sales', controllers.patchSales);
 app.post('/api/sales', controllers.postSales);
+app.patch('/api/sales', controllers.patchSales);
+
+app.all('*', (req, res) => {
+  res.status(404).send({ msg: 'invalid URL' });
+});
 
 app.use(errorHandlers.customError);
 app.use(errorHandlers.psqlErrors);
